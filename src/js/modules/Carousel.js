79cc7652f44010e1b11acc37a5ce30a5e49e87e3
Jamie Boyd <enkizero@gmail.com>
2018-07-17 14:56:50 +0100

    Added skeleton of carousel child classes

--- src/js/modules/Carousel.js
+++ src/js/modules/Carousel.js
@@ -12,14 +12,25 @@ const selectors =  {
  */
 class Carousel {
   constructor(element) {
-    this.domElement = element;
+    this.compDOMElement = element;
+  }
+
+  bindCustomMessageEvents() {
+    this.compDOMElement.addEventListener('toggleShowHide', this.toggleControl.bind(this));
   }
 }
 
 /**
  * FadeCarousel - Class representing a Fade-type Carousel DOM component
  */
-class FadeCarousel extends Carousel {
+class Fader extends Carousel {
+  constructor(element) {
+    super(element);
+  }
+}
+
+
+class Scroll extends Carousel {
   constructor(element) {
     super(element);
   }
